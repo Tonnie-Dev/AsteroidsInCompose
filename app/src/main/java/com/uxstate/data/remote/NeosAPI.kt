03415ab07e83b4a797774d92fd514fda5109c854
Tonnie <vontonnie@gmail.com>
2022-06-04 18:57:30 +0300

    Add getNearEarthObjects NeosAPI function

--- app/src/main/java/com/uxstate/data/remote/NeosAPI.kt
+++ app/src/main/java/com/uxstate/data/remote/NeosAPI.kt
@@ -1,8 +1,24 @@
 package com.uxstate.data.remote
 
+import com.uxstate.data.remote.dto.SomeDTO
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface NeosAPI {
 
+    @GET("neo/rest/v1/feed")
+    suspend fun getNearEarthObjects(
+
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("api_key") apiKey: String
+
+    ):List<SomeDTO>
 
 
+    companion object {
 
+        const val BASE_URL = "https://api.nasa.gov/"
+        const val API_KEY = "Cnkc0VTeJQklSu79us3xzoRCmhjCIa9nsXpLdjs6"
+    }
 }
---
+++ app/src/main/java/com/uxstate/data/remote/dto/SomeDTO.kt
@@ -0,0 +1,3 @@
+package com.uxstate.data.remote.dto
+
+data class SomeDTO(val name:String)
