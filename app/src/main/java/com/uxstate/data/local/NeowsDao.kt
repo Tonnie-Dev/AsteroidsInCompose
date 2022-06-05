27134e6ef02ae72149dc75ec1d1dc7a3f38134ef
Tonnie <vontonnie@gmail.com>
2022-06-05 15:35:57 +0300

    Add clearNeows() dao function

--- app/src/main/java/com/uxstate/data/local/NeowsDao.kt
+++ app/src/main/java/com/uxstate/data/local/NeowsDao.kt
@@ -3,11 +3,15 @@ package com.uxstate.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface NeowsDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNeows(neows:List<NeowsEntity>)
+
+    @Query("DELETE from neowsentity")
+    suspend fun clearNeows()
 
 }
