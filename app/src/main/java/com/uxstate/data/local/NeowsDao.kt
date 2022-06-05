d8472b0df3ad0d239af4d7e55ad99ec4266cdd73
Tonnie <vontonnie@gmail.com>
2022-06-05 15:33:40 +0300

    Add insertNeows() Dao function

--- app/src/main/java/com/uxstate/data/local/NeowsDao.kt
+++ app/src/main/java/com/uxstate/data/local/NeowsDao.kt
@@ -1,9 +1,13 @@
 package com.uxstate.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface NeowsDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNeows(neows:List<NeowsEntity>)
 
 }
