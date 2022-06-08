1569bf74b72e997ce96b5840fae75517e5107f62
Tonnie <vontonnie@gmail.com>
2022-06-08 09:04:38 +0300

    Add @Provides NeowsDatabase module function

--- app/src/main/java/com/uxstate/data/local/NeowsDatabase.kt
+++ app/src/main/java/com/uxstate/data/local/NeowsDatabase.kt
@@ -5,4 +5,9 @@ import androidx.room.RoomDatabase
 abstract class NeowsDatabase : RoomDatabase(){
 
 abstract val dao:NeowsDao
+
+companion object{
+
+    const val DB_NAME = "NeowsDatabase"
+}
 }
--- app/src/main/java/com/uxstate/di/AppModule.kt
+++ app/src/main/java/com/uxstate/di/AppModule.kt
@@ -1,10 +1,25 @@
 package com.uxstate.di
 
+import android.content.Context
+import androidx.room.Room
+import com.uxstate.data.local.NeowsDatabase
+import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-@Singleton
+@Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideNeowsDatabase(@ApplicationContext context: Context): NeowsDatabase {
+
+        return Room.databaseBuilder(context, NeowsDatabase::class.java, NeowsDatabase.DB_NAME)
+                .build()
+
+    }
 }
