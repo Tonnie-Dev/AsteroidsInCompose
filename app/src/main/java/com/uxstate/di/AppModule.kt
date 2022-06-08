00a75e588675c283f773e2e9fa2587fdc0f81853
Tonnie <vontonnie@gmail.com>
2022-06-08 09:10:32 +0300

    Add provideNeowsAPI module function

--- app/src/main/java/com/uxstate/di/AppModule.kt
+++ app/src/main/java/com/uxstate/di/AppModule.kt
@@ -3,23 +3,39 @@ package com.uxstate.di
 import android.content.Context
 import androidx.room.Room
 import com.uxstate.data.local.NeowsDatabase
+import com.uxstate.data.remote.NeowsAPI
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    @Singleton
+
     @Provides
+    @Singleton
     fun provideNeowsDatabase(@ApplicationContext context: Context): NeowsDatabase {
 
         return Room.databaseBuilder(context, NeowsDatabase::class.java, NeowsDatabase.DB_NAME)
                 .build()
 
     }
+
+    @Provides
+    @Singleton
+    fun provideNeowsAPI(): NeowsAPI {
+
+        return Retrofit.Builder()
+                .baseUrl(NeowsAPI.BASE_URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create(NeowsAPI::class.java)
+    }
+
 }
