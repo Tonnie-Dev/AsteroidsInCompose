17c64d8e802f1b737eb158ec261b265164256285
Tonnie <vontonnie@gmail.com>
2022-06-10 19:53:11 +0300

    Add extra Picture specific states

--- app/src/main/java/com/uxstate/presentation/overview/OverviewState.kt
+++ app/src/main/java/com/uxstate/presentation/overview/OverviewState.kt
@@ -1,10 +1,14 @@
 package com.uxstate.presentation.overview
 
+import com.uxstate.domain.model.AstroPicture
 import com.uxstate.domain.model.NearEarthObject
 
 data class OverviewState(
     val neows: List<NearEarthObject> = emptyList(),
     val isLoading: Boolean = false,
     val isRefreshing: Boolean = false,
-val errorMessage:String? = null
+    val errorMessage: String? = null,
+    val astroPicture: List<AstroPicture> = emptyList(),
+    val isPictureLoading: Boolean = false
+
 )
