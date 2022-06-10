4dbdd0527f594fa090d8a7834a0984a5bc6972e0
Tonnie <vontonnie@gmail.com>
2022-06-10 11:50:20 +0300

    Add OverviewScreen

--- app/src/main/java/com/uxstate/presentation/overview/OverviewScreen.kt
+++ app/src/main/java/com/uxstate/presentation/overview/OverviewScreen.kt
@@ -1,19 +1,24 @@
 package com.uxstate.presentation.overview
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.uxstate.R
+import com.uxstate.presentation.overview.components.ButtonItem
 import com.uxstate.presentation.overview.components.NeowsItem
 import java.time.LocalDate
-import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 @Destination(start = true)
@@ -23,18 +28,49 @@ fun OverviewScreen(
     navigator: DestinationsNavigator
 ) {
 
-
     val state = viewModel.state
     val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = state.isRefreshing)
 
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+            modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp)
+    ) {
 
         //IMAGE
 
+        Box(
+                modifier = Modifier
+
+                        .weight(3f)
+        ) {
+
+            Image(
+                    painter = painterResource(id = R.drawable.asteroid_hazardous),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+            )
+        }
+
         //BUTTON ROW
 
-        SwipeRefresh(state = swipeRefreshState, onRefresh = {
+        Row(
+                modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+        ) {
+            ButtonItem(onClickButton = { /*TODO*/ }, text = "Today")
+            ButtonItem(onClickButton = { /*TODO*/ }, text = "Weekly")
+            ButtonItem(onClickButton = { /*TODO*/ }, text = "Monthly")
+        }
+
+        SwipeRefresh(state = swipeRefreshState, modifier = Modifier
+                .fillMaxWidth()
+                .weight(6f), onRefresh = {
 
             viewModel.onEvent(OverviewEvent.Refreshing)
         }) {
@@ -42,9 +78,10 @@ fun OverviewScreen(
             LazyColumn(content = {
 
 
-                items(state.neows){
+                items(state.neows) {
 
                     NeowsItem(
+                            modifier = Modifier.fillMaxWidth(),
                             name = it.codename,
                             approachDate = localDateToString(it.closeApproachDate),
                             isHazardous = it.isPotentiallyHazardous,
@@ -60,7 +97,7 @@ fun OverviewScreen(
 
 }
 
-fun localDateToString(date: LocalDate):String {
+fun localDateToString(date: LocalDate): String {
 
     val pattern = "dd-MM-yyyy"
     val dateFormatter = DateTimeFormatter.ofPattern(pattern)
