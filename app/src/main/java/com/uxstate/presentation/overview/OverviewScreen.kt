18acf956a77657acf9a2e10e6dfb3a388f11f0fb
Tonnie <vontonnie@gmail.com>
2022-06-10 09:09:25 +0300

    Add OverviewScreen code shell

--- app/src/main/java/com/uxstate/presentation/overview/OverviewScreen.kt
+++ app/src/main/java/com/uxstate/presentation/overview/OverviewScreen.kt
@@ -1,10 +1,23 @@
 package com.uxstate.presentation.overview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination(start = true)
 @Composable
-fun OverviewScreen() {
+fun OverviewScreen(viewModel: OverviewViewModel = hiltViewModel(), navigator: DestinationsNavigator) {
+
+
+    val state = viewModel.state
+
+
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
 
 }
