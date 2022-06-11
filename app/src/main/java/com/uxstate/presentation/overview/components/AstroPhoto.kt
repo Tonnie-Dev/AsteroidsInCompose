9f5bb7c388705b8e24077987af970bf35e3ef1ee
Tonnie <vontonnie@gmail.com>
2022-06-11 20:21:15 +0300

    Add onTapPhoto parameter to AstroPhoto

--- app/src/main/java/com/uxstate/presentation/overview/components/AstroPhoto.kt
+++ app/src/main/java/com/uxstate/presentation/overview/components/AstroPhoto.kt
@@ -3,9 +3,9 @@ package com.uxstate.presentation.overview.components
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
 import androidx.compose.animation.graphics.res.animatedVectorResource
-import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
@@ -18,19 +18,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
-import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
-import coil.request.ImageRequest
-import coil.size.Size
 import com.uxstate.R
 import com.uxstate.domain.model.AstroPicture
 
 @OptIn(ExperimentalAnimationGraphicsApi::class)
 @Composable
-fun AstroPhoto(picture: AstroPicture, modifier: Modifier = Modifier, isExpanded:Boolean) {
+fun AstroPhoto(
+    picture: AstroPicture,
+    modifier: Modifier = Modifier,
+    isExpanded: Boolean,
+    onTapPhoto: () -> Unit
+) {
     val image = AnimatedImageVector.animatedVectorResource(R.drawable.loading_animation)
     val atEnd by remember { mutableStateOf(true) }
 
@@ -39,62 +40,63 @@ fun AstroPhoto(picture: AstroPicture, modifier: Modifier = Modifier, isExpanded:
             .scheme("https")
             .build()
 
-Column {
+    Column {
 
-    Box(modifier = modifier) {
+        Box(modifier = modifier.clickable { onTapPhoto() }) {
 
-        /*  AsyncImage(
-                  model = ImageRequest.Builder(LocalContext.current)
-                          .data(imgUri)
-                          .crossfade(true)
-                          .size(Size.ORIGINAL)
-                          .build(),
-                  placeholder = rememberAnimatedVectorPainter(image, atEnd),
-                  contentDescription = picture.title,
-                  contentScale = ContentScale.FillWidth,
-                  modifier = modifier
-                          .align(Alignment.Center)
-                          .size(420.dp, 240.dp)
-                          .padding(20.dp)
-          )
-  */
-        Image(
-                painter = rememberImagePainter(
-                        data = imgUri ,
-                        builder = { crossfade(true)
-                            placeholder(R.drawable.loading_animation)
+            /*  AsyncImage(
+                      model = ImageRequest.Builder(LocalContext.current)
+                              .data(imgUri)
+                              .crossfade(true)
+                              .size(Size.ORIGINAL)
+                              .build(),
+                      placeholder = rememberAnimatedVectorPainter(image, atEnd),
+                      contentDescription = picture.title,
+                      contentScale = ContentScale.FillWidth,
+                      modifier = modifier
+                              .align(Alignment.Center)
+                              .size(420.dp, 240.dp)
+                              .padding(20.dp)
+              )
+      */
+            Image(
+                    painter = rememberImagePainter(
+                            data = imgUri,
+                            builder = {
+                                crossfade(true)
+                                placeholder(R.drawable.loading_animation)
 
 
-                        }
-                ),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                        .align(Alignment.Center)
-                        .size(420.dp, 240.dp)
-                        .padding(8.dp)
+                            }
+                    ),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(420.dp, 240.dp)
+                            .padding(8.dp)
 
-        )
-        Surface(
-                color = Color(0x7F000000),
-                shape = RoundedCornerShape(20.dp),
-                modifier = modifier
-                        .align(Alignment.BottomCenter)
-                        .padding(16.dp)
-        ) {
+            )
+            Surface(
+                    color = Color(0x7F000000),
+                    shape = RoundedCornerShape(20.dp),
+                    modifier = modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(16.dp)
+            ) {
 
-            Text(text = picture.title, color = Color.White, modifier = Modifier.padding(8.dp))
+                Text(text = picture.title, color = Color.White, modifier = Modifier.padding(8.dp))
+            }
+
         }
 
-    }
-    
-    AnimatedVisibility(visible = isExpanded) {
-        
-        Row() {
-            Text(text = picture.explanation)
+        AnimatedVisibility(visible = isExpanded) {
+
+            Row() {
+                Text(text = picture.explanation)
+            }
         }
     }
-}
 
 }
 
