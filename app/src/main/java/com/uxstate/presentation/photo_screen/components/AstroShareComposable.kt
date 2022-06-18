465551c8cd72f6db04017cc539bfe7481ec431da
Tonnie <vontonnie@gmail.com>
2022-06-18 07:15:03 +0300

    Add Card Container in AstroShareComposable

--- app/src/main/java/com/uxstate/presentation/photo_screen/components/AstroShareComposable.kt
+++ app/src/main/java/com/uxstate/presentation/photo_screen/components/AstroShareComposable.kt
@@ -1,24 +1,13 @@
 package com.uxstate.presentation.photo_screen.components
 
-import android.widget.Space
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
 import coil.compose.rememberImagePainter
@@ -26,43 +15,54 @@ import com.uxstate.R
 import com.uxstate.domain.model.AstroPicture
 
 
+
+@ExperimentalMaterial3Api
 @Composable
-fun AstroShareComposable(picture: AstroPicture) {
+fun AstroShareComposable(picture: AstroPicture, modifier: Modifier = Modifier) {
+
+
+    Card(
+            modifier = modifier,
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
+            shape = MaterialTheme.shapes.large
+    ) {
+
+    }
     val imgUri = picture.url.toUri()
             .buildUpon()
             .scheme("https")
             .build()
-Box() {
-    Column(modifier = Modifier.align(Alignment.TopStart)) {
+    Box() {
+        Column(modifier = Modifier.align(Alignment.TopStart)) {
 
 
-        Image(
-                painter = rememberImagePainter(
-                        data = imgUri,
-                        builder = {
-                            crossfade(true)
-                            placeholder(R.drawable.loading_animation)
-                        }
-                ),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
+            Image(
+                    painter = rememberImagePainter(
+                            data = imgUri,
+                            builder = {
+                                crossfade(true)
+                                placeholder(R.drawable.loading_animation)
+                            }
+                    ),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
 
-                        .size(420.dp, 240.dp)
-                        .padding(8.dp)
+                            .size(420.dp, 240.dp)
+                            .padding(8.dp)
 
-        )
+            )
 
-        //Title
-        Spacer(modifier = Modifier.height(16.dp))
+            //Title
+            Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = picture.title, fontWeight = Bold)
-        Spacer(modifier = Modifier.height(16.dp))
-        //About
-        Text(text = picture.explanation, softWrap = true, modifier = Modifier.width(400.dp))
+            Text(text = picture.title, fontWeight = Bold)
+            Spacer(modifier = Modifier.height(16.dp))
+            //About
+            Text(text = picture.explanation, softWrap = true, modifier = Modifier.width(400.dp))
+
+        }
 
     }
-
-}
 
 }
