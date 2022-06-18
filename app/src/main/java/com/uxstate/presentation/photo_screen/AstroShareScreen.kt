6f4e80eb3b7c9d5d21714c376f9082d25bb30bf8
Tonnie <vontonnie@gmail.com>
2022-06-18 07:19:18 +0300

    Add Image into the card composable for AstroPhotoComposable

--- app/src/main/java/com/uxstate/presentation/photo_screen/AstroShareScreen.kt
+++ app/src/main/java/com/uxstate/presentation/photo_screen/AstroShareScreen.kt
@@ -1,10 +1,12 @@
 package com.uxstate.presentation.photo_screen
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
 import com.uxstate.domain.model.AstroPicture
 import com.uxstate.presentation.photo_screen.components.AstroShareComposable
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun AstroShareScreen(astroPicture: AstroPicture) {
--- app/src/main/java/com/uxstate/presentation/photo_screen/components/AstroShareComposable.kt
+++ app/src/main/java/com/uxstate/presentation/photo_screen/components/AstroShareComposable.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
@@ -20,49 +21,35 @@ import com.uxstate.domain.model.AstroPicture
 @Composable
 fun AstroShareComposable(picture: AstroPicture, modifier: Modifier = Modifier) {
 
-
+    val imgUri = picture.url.toUri()
+            .buildUpon()
+            .scheme("https")
+            .build()
     Card(
             modifier = modifier,
             colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceVariant),
             shape = MaterialTheme.shapes.large
     ) {
 
+        //Image
+        Image(
+                painter = rememberImagePainter(
+                        data = imgUri,
+                        builder = {
+                            crossfade(true)
+                            placeholder(R.drawable.loading_animation)
+                        }
+                ),
+                contentDescription = picture.title,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                        .clip(MaterialTheme.shapes.large)
+                        .size(420.dp, 240.dp)
+                        .padding(8.dp)
+
+        )
     }
-    val imgUri = picture.url.toUri()
-            .buildUpon()
-            .scheme("https")
-            .build()
-    Box() {
-        Column(modifier = Modifier.align(Alignment.TopStart)) {
 
 
-            Image(
-                    painter = rememberImagePainter(
-                            data = imgUri,
-                            builder = {
-                                crossfade(true)
-                                placeholder(R.drawable.loading_animation)
-                            }
-                    ),
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-
-                            .size(420.dp, 240.dp)
-                            .padding(8.dp)
-
-            )
-
-            //Title
-            Spacer(modifier = Modifier.height(16.dp))
-
-            Text(text = picture.title, fontWeight = Bold)
-            Spacer(modifier = Modifier.height(16.dp))
-            //About
-            Text(text = picture.explanation, softWrap = true, modifier = Modifier.width(400.dp))
-
-        }
-
-    }
 
 }
