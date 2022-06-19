9f125e6b1814a86f3ba94022edfc470199018e4b
Tonnie <vontonnie@gmail.com>
2022-06-19 14:19:41 +0300

    Add getFavoriteAstroPhotos dao function

--- app/src/main/java/com/uxstate/data/local/AstroDao.kt
+++ app/src/main/java/com/uxstate/data/local/AstroDao.kt
@@ -3,12 +3,16 @@ package com.uxstate.data.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface AstroDao {
 
 @Insert(onConflict = OnConflictStrategy.REPLACE)
 fun insertFavoriteAstroPhoto(photoEntity: AstroPhotoEntity)
+
+@Query("SELECT * FROM astrophotoentity ")
+fun getFavoriteAstroPhotos():List<AstroPhotoEntity>
 
 
 }
