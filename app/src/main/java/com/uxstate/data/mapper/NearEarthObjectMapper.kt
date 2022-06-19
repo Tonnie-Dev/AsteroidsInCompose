a6db89be942f08ec983a5de3d683eebeb5c3e599
Tonnie <vontonnie@gmail.com>
2022-06-19 12:46:29 +0300

    Delete NeowsEntity

--- app/src/main/java/com/uxstate/data/mapper/NearEarthObjectMapper.kt
+++ app/src/main/java/com/uxstate/data/mapper/NearEarthObjectMapper.kt
@@ -1,12 +1,7 @@
 package com.uxstate.data.mapper
 
-import com.uxstate.data.local.NeowsEntity
 import com.uxstate.data.remote.dto.AstroPhotoDTO
 import com.uxstate.domain.model.AstroPicture
-import com.uxstate.domain.model.NearEarthObject
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.*
 
 
 //Picture DTO to Picture Model
