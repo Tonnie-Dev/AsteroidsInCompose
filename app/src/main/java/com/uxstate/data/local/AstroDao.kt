5dd2d53b3000b8745052128ad90a40cd8b776c09
Tonnie <vontonnie@gmail.com>
2022-06-19 14:12:18 +0300

    Add insertFavoriteAstroPhoto in the dao

--- app/src/main/java/com/uxstate/data/local/AstroDao.kt
+++ app/src/main/java/com/uxstate/data/local/AstroDao.kt
@@ -1,11 +1,14 @@
 package com.uxstate.data.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 
 @Dao
 interface AstroDao {
 
-
+@Insert(onConflict = OnConflictStrategy.REPLACE)
+fun insertFavoriteAstroPhoto(photoEntity: AstroPhotoEntity)
 
 
 }
