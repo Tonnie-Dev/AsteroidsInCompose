ca88ebe2154b3849cb5cac5937e09e393b9203ba
Tonnie <vontonnie@gmail.com>
2022-06-22 08:10:53 +0300

    Add CenterAlignedTopAppBar inside FavoritePhotosScreen

--- app/src/main/java/com/uxstate/presentation/fav_photos_screen/FavoritePhotosScreen.kt
+++ app/src/main/java/com/uxstate/presentation/fav_photos_screen/FavoritePhotosScreen.kt
@@ -1,67 +1,75 @@
 package com.uxstate.presentation.fav_photos_screen
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.uxstate.R
 import com.uxstate.presentation.components.AstroPhotoComposable
-import com.uxstate.presentation.components.AstroShareComposable
 import com.uxstate.presentation.destinations.PhotoDetailsScreenDestination
-import com.uxstate.presentation.detail_screen.PhotoDetailsScreen
-import com.uxstate.presentation.overview_screen.OverviewViewModel
 import com.uxstate.util.LocalSpacing
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
-fun FavoritePhotosScreen(viewModel: FavPhotosViewModel = hiltViewModel(), navigator: DestinationsNavigator) {
+fun FavoritePhotosScreen(
+    viewModel: FavPhotosViewModel = hiltViewModel(),
+    navigator: DestinationsNavigator
+) {
     val spacing = LocalSpacing.current
     val photos = viewModel.state.favoritePhotosList
 
-Scaffold() { paddingValues ->
+    Scaffold(topBar = {
+      CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        titleContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                ),
+                title = {
 
-    if (photos.isNotEmpty()){
-        LazyColumn( contentPadding = paddingValues, content = {
+                    Text(text = stringResource(id = R.string.fav_header))
+                }
+        )
 
-        items(photos) { photo ->
 
-        AstroPhotoComposable(picture = photo, onTapPhoto = {
-            navigator.navigate(PhotoDetailsScreenDestination(photo = photo)) }) {
+    }) { paddingValues ->
+
+        if (photos.isNotEmpty()) {
+            LazyColumn(contentPadding = paddingValues, content = {
+
+                items(photos) { photo ->
+
+                    AstroPhotoComposable(picture = photo, onTapPhoto = {
+                        navigator.navigate(PhotoDetailsScreenDestination(photo = photo))
+                    }) {
+
+                    }
+                }
+            })
+        } else {
+
+            Box(modifier = Modifier.fillMaxSize()) {
+
+                Text(
+                        text = stringResource(id = R.string.no_fav_photos),
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.align(Alignment.Center)
+                )
+            }
 
         }
-        }
-    })}else{
 
-        Box(modifier = Modifier.fillMaxSize()) {
-
-            Text(
-                    text = stringResource(id = R.string.no_fav_photos),
-                    style = MaterialTheme.typography.headlineLarge,
-                    color = MaterialTheme.colorScheme.error, modifier = Modifier.align(Alignment.Center)
-            )
-        }
 
     }
-
-
-
-
-}
 
 
 }
