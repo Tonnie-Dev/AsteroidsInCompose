b98c34c06bf679e9677d904f0b0b4a232f58cda9
Tonnie <vontonnie@gmail.com>
2022-06-25 07:02:31 +0300

    Add FavPhotoEntity class

---
+++ app/src/main/java/com/uxstate/data/local/FavPhotoEntity.kt
@@ -0,0 +1,13 @@
+package com.uxstate.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class FavPhotoEntity(  @PrimaryKey
+                            val id: String,
+                            val title: String,
+                            val mediaType:String,
+                            val explanation: String,
+                            val url: String,
+                            val isFavorite:Boolean)
