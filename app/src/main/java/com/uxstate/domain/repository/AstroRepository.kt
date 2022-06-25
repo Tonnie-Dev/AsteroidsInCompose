3dc47d81f033003bbd97a02757c65f9e9d055991
Tonnie <vontonnie@gmail.com>
2022-06-25 14:54:02 +0300

    Remove insertAstroPhotos repository definition function

--- app/src/main/java/com/uxstate/domain/repository/AstroRepository.kt
+++ app/src/main/java/com/uxstate/domain/repository/AstroRepository.kt
@@ -5,11 +5,17 @@ import com.uxstate.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 
-interface AstroRepository {
+interface AstroRepository
+
+{
+
+    /*Put functions to access data or manage data - this operations should
+    be the ones done from the UI not automatic (e.g. populating remote
+    data into db should not be a function of repository definition)*/
 
     //ASTROPHOTOENTITY
     fun fetchAstroPhotos(fetchFromRemote: Boolean): Flow<Resource<List<AstroPhoto>>>
-    suspend fun insertAstroPhotos(astroPhotos: List<AstroPhoto>)
+
 
 
     //FAVPHOTOENTITY
