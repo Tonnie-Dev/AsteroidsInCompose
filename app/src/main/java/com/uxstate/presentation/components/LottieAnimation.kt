274f0b86bdd87dd6fe5d763da4d04157c9e2fb16
Tonnie <vontonnie@gmail.com>
2022-07-05 19:27:48 +0300

    Add LottieAnimationFile

--- app/src/main/java/com/uxstate/presentation/components/LottieAnimation.kt
+++ app/src/main/java/com/uxstate/presentation/components/LottieAnimation.kt
@@ -1,12 +1,19 @@
 package com.uxstate.presentation.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.uxstate.R
 
 @Composable
-fun LottieAnimation(onClick: () -> Unit) {
+fun LottieAnimationComposable(onClick: () -> Unit, modifier: Modifier = Modifier) {
 
 
     Column(
@@ -14,7 +21,29 @@ fun LottieAnimation(onClick: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        //spec - hold reference to animation object
+        val spec = LottieCompositionSpec.RawRes(R.raw.delete_animation)
+        //state - manipulate progress
+        var isDelete by remember {
+            mutableStateOf(false)
+        }
 
+        //progress
+
+        val progress by animateFloatAsState(targetValue = if (isDelete) 1f else 0f)
+        //composition - render animation
+
+        val composition by rememberLottieComposition(spec = spec)
+
+        //lottie animation composable
+
+        LottieAnimation(
+                composition = composition,
+                progress = progress,
+                Modifier.clickable { onClick()
+                isDelete = !isDelete
+                }
+        )
 
     }
 }
