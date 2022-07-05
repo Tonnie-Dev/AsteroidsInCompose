21415652d625e965ad4ed5c172b87e840efbaaf7
Tonnie <vontonnie@gmail.com>
2022-07-05 19:08:41 +0300

    Add Delete json animation file

---
+++ app/src/main/java/com/uxstate/presentation/components/LottieAnimation.kt
@@ -0,0 +1,20 @@
+package com.uxstate.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun LottieAnimation(onClick: () -> Unit) {
+
+
+    Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+
+
+    }
+}
