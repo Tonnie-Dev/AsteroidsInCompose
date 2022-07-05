875a7224174ae61b3532454c819df354d5e8bcb3
Tonnie <vontonnie@gmail.com>
2022-07-05 20:29:44 +0300

    Add Lottie Animation Placeholder

--- app/src/main/java/com/uxstate/presentation/components/LottieAnimation.kt
+++ app/src/main/java/com/uxstate/presentation/components/LottieAnimation.kt
@@ -1,50 +1,37 @@
 package com.uxstate.presentation.components
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import com.airbnb.lottie.compose.*
-import com.uxstate.R
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+
+
+
+//LottieAnimation
+@Composable
+fun LottieAnimationComposable(
+    modifier: Modifier, lottie: Int
+) {
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(lottie))
+    val progress by animateLottieCompositionAsState(composition = composition)
+
+    LottieAnimation(composition = composition,
+            progress = progress,
+            modifier = modifier.size(80.dp))
+}
+
+
+//Lottie Animation Placeholder
 
 @Composable
-fun LottieAnimationComposable(onClick: () -> Unit, modifier: Modifier = Modifier) {
+fun LottieAnimationPlaceHolder(modifier: Modifier, lottie: Int) {
 
+    LottieAnimationComposable(modifier = modifier, lottie = lottie)
 
-    Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier
-    ) {
-
-        //spec - hold reference to animation object
-        val spec = LottieCompositionSpec.RawRes(R.raw.delete_animation)
-
-
-        //state - manipulate progress
-
-
-       /* var isDelete by remember {
-            mutableStateOf(false)
-        }
-
-        //progress
-
-        val progress by animateFloatAsState(targetValue = if (isDelete) 1f else .5f)*/
-        //composition - render animation
-
-        val composition by rememberLottieComposition(spec = spec)
-        val state = animateLottieCompositionAsState(composition = composition, iterations = LottieConstants.IterateForever)
-        //lottie animation composable
-
-        LottieAnimation(
-                composition = composition,
-                progress = state.progress,
-               
-        )
-
-    }
 }
