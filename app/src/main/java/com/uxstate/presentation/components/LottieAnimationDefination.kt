4f197281dc8e196d3bce4e6ac664b0c4f90d0eca
Tonnie <vontonnie@gmail.com>
2022-07-05 20:47:30 +0300

    Add delete_red_icon lottie json file

--- app/src/main/java/com/uxstate/presentation/components/LottieAnimationDefination.kt
+++ app/src/main/java/com/uxstate/presentation/components/LottieAnimationDefination.kt
@@ -5,12 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.animateLottieCompositionAsState
-import com.airbnb.lottie.compose.rememberLottieComposition
-
-
+import com.airbnb.lottie.compose.*
 
 
 //LottieAnimation
@@ -19,11 +14,16 @@ fun LottieAnimationComposable(
     modifier: Modifier, lottie: Int
 ) {
     val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(lottie))
-    val progress by animateLottieCompositionAsState(composition = composition)
+    val progress by animateLottieCompositionAsState(
+            composition = composition,
+            iterations = LottieConstants.IterateForever
+    )
 
-    LottieAnimation(composition = composition,
+    LottieAnimation(
+            composition = composition,
             progress = progress,
-            modifier = modifier.size(80.dp))
+            modifier = modifier.size(50.dp)
+    )
 }
 
 
