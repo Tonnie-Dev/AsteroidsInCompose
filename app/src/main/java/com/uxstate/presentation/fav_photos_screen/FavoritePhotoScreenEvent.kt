b1b6059f66ab62b8130bb91718b770eebe9422a2
Tonnie <vontonnie@gmail.com>
2022-07-09 19:19:38 +0300

    Add extra events for FavoritePhotoScreenEvent sealed class

--- app/src/main/java/com/uxstate/presentation/fav_photos_screen/FavoritePhotoScreenEvent.kt
+++ app/src/main/java/com/uxstate/presentation/fav_photos_screen/FavoritePhotoScreenEvent.kt
@@ -1,10 +1,14 @@
 package com.uxstate.presentation.fav_photos_screen
 
 import com.uxstate.domain.model.AstroPhoto
+import com.uxstate.util.PhotoDateFilter
 
 sealed class FavoritePhotoScreenEvent {
 
 
 
     data class OnRemoveFromFavorite(val photo:AstroPhoto):FavoritePhotoScreenEvent()
+    data class OnClickTodayPhotos(val dateFilter: PhotoDateFilter)
+    data class OnClickRecentPhotos(val dateFilter: PhotoDateFilter)
+    data class OnClickAllPhotos(val dateFilter: PhotoDateFilter)
 }
