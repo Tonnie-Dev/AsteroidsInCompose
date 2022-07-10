5a5ba92befcc3c7ff0bdc7c7483ae252e579b8f9
Tonnie <vontonnie@gmail.com>
2022-07-10 07:43:50 +0300

    Add OnRestoreDeletedPhoto subclass of FavoritePhotoScreenEvent

--- app/src/main/java/com/uxstate/presentation/fav_photos_screen/FavoritePhotoScreenEvent.kt
+++ app/src/main/java/com/uxstate/presentation/fav_photos_screen/FavoritePhotoScreenEvent.kt
@@ -6,9 +6,9 @@ import com.uxstate.util.PhotoDateFilter
 sealed class FavoritePhotoScreenEvent {
 
 
-
-    data class OnRemoveFromFavorite(val photo:AstroPhoto):FavoritePhotoScreenEvent()
-    data class OnClickTodayPhotos(val dateFilter: PhotoDateFilter):FavoritePhotoScreenEvent()
-    data class OnClickRecentPhotos(val dateFilter: PhotoDateFilter):FavoritePhotoScreenEvent()
-    data class OnClickAllPhotos(val dateFilter: PhotoDateFilter):FavoritePhotoScreenEvent()
+    data class OnRemoveFromFavorite(val photo: AstroPhoto) : FavoritePhotoScreenEvent()
+    data class OnClickTodayPhotos(val dateFilter: PhotoDateFilter) : FavoritePhotoScreenEvent()
+    data class OnClickRecentPhotos(val dateFilter: PhotoDateFilter) : FavoritePhotoScreenEvent()
+    data class OnClickAllPhotos(val dateFilter: PhotoDateFilter) : FavoritePhotoScreenEvent()
+    object OnRestoreDeletedPhoto : FavoritePhotoScreenEvent()
 }
