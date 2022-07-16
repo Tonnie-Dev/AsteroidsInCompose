669adcbc4d53eb5a47047156f07470c902e2a2e9
Tonnie <vontonnie@gmail.com>
2022-07-16 15:11:53 +0300

    Delete GetFavAstroPhotoUseCase

--- app/src/main/java/com/uxstate/domain/use_cases/UseCaseContainer.kt
+++ app/src/main/java/com/uxstate/domain/use_cases/UseCaseContainer.kt
@@ -2,11 +2,9 @@ package com.uxstate.domain.use_cases
 
 data class UseCaseContainer(
     val deleteFavoritePhotoUseCase: DeleteFavoritePhotoUseCase,
-    val getFavAstroPhotoUseCase: GetFavAstroPhotoUseCase,
     val getAstroPhotosUseCase: GetAstroPhotosUseCase,
     val getFavAstroPhotosUseCase:GetFavAstroPhotosUseCase,
     val insertAstroPhotoUseCase: InsertAstroPhotoUseCase,
-    val checkIfPhotoIsInDatabaseUseCase: CheckIfPhotoIsInDatabaseUseCase,
     val updateIsFavoriteStatus: UpdateIsFavoriteStatusUseCase,
     val getLiveAstroPhotosUseCase: GetLiveAstroPhotosUseCase
 )
