c70e9009711c79d3a8cec78de315dbfb05213ac1
Tonnie <vontonnie@gmail.com>
2022-07-17 11:10:02 +0300

    Mark AboutScreen() with @Destination

--- app/src/main/java/com/uxstate/presentation/about_screen/AboutScreen.kt
+++ app/src/main/java/com/uxstate/presentation/about_screen/AboutScreen.kt
@@ -10,9 +10,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ramcosta.composedestinations.annotation.Destination
 import com.uxstate.R
 import com.uxstate.presentation.components.LottieAnimationPlaceHolder
 
+@Destination
 @Composable
 fun AboutScreen() {
     Column(
