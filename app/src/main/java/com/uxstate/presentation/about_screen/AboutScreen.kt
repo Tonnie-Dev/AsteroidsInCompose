6dfa9ce7ecf14bd78f9e3107ee0dcd5107bfdc1f
Tonnie <vontonnie@gmail.com>
2022-07-17 10:53:05 +0300

    Remove Animation files from drawable to raw directory

--- app/src/main/java/com/uxstate/presentation/about_screen/AboutScreen.kt
+++ app/src/main/java/com/uxstate/presentation/about_screen/AboutScreen.kt
@@ -2,12 +2,21 @@ package com.uxstate.presentation.about_screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.uxstate.R
+import com.uxstate.presentation.components.LottieAnimationPlaceHolder
 
 @Composable
 fun AboutScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
-
+    Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+Text(text = "Welcome")
+        
+        LottieAnimationPlaceHolder(lottie = R.raw.galaxy_anim)
     }
 }
