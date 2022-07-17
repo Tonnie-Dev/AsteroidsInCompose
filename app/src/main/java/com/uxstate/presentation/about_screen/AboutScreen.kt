97b46e9e6754f9bd2f3286f96a71709e0da12e6f
Tonnie <vontonnie@gmail.com>
2022-07-17 11:49:39 +0300

    Style AboutScreen Texts

--- app/src/main/java/com/uxstate/presentation/about_screen/AboutScreen.kt
+++ app/src/main/java/com/uxstate/presentation/about_screen/AboutScreen.kt
@@ -1,14 +1,16 @@
 package com.uxstate.presentation.about_screen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.annotation.Destination
 import com.uxstate.R
@@ -19,16 +21,27 @@ import com.uxstate.presentation.components.LottieAnimationPlaceHolder
 fun AboutScreen() {
     Column(
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
     ) {
-        Text(text = "Welcome")
+        Text(
+                text = stringResource(id = R.string.welcome_message),
+                style = MaterialTheme.typography.headlineMedium,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
 
         LottieAnimationPlaceHolder(
                 lottie = R.raw.galaxy_anim,
                 modifier = Modifier.size(300.dp)
         )
 
-        Text(text = stringResource(id = R.string.welcome_header))
+        Text(
+                text = stringResource(id = R.string.welcome_header),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
     }
 }
 
