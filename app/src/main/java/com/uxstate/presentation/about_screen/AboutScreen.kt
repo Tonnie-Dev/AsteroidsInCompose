e3cec64fd921fe8880d08e41e524941b22dae88b
Tonnie <vontonnie@gmail.com>
2022-07-17 10:49:28 +0300

    Add AboutScreen composable

---
+++ app/src/main/java/com/uxstate/presentation/about_screen/AboutScreen.kt
@@ -0,0 +1,13 @@
+package com.uxstate.presentation.about_screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AboutScreen() {
+    Column(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
