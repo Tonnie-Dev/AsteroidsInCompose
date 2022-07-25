5318cf5df1ce645c117eb3c6c7a141f109a2e735
Tonnie <vontonnie@gmail.com>
2022-07-25 19:53:32 +0300

    Refactor PhotoDateFilter class constructor to endDate Filter

--- app/src/main/java/com/uxstate/util/PhotoDateFilter.kt
+++ app/src/main/java/com/uxstate/util/PhotoDateFilter.kt
@@ -5,21 +5,33 @@ import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.ZoneOffset
 
-sealed class PhotoDateFilter(val startDate: LocalDateTime) {
+sealed class PhotoDateFilter(val startDate: LocalDateTime, val endDate: LocalDateTime) {
 
+    //start at 12:00 AM, end at current time
     object TodayPhotos : PhotoDateFilter(
-            startDate = LocalDateTime.of(LocalDate.now(), LocalTime.MIN)
+            startDate = LocalDateTime.of(LocalDate.now(), LocalTime.MIN),
+            endDate = LocalDateTime.now()
     )
 
+    //start 4 days prior but excluding today, end yesterday 11:59 PM
     object RecentPhotos : PhotoDateFilter(
             startDate = LocalDateTime.now()
-                    .minusDays(4).minusHours(24)
+                    .minusDays(4)
+                    .minusHours(24),
+
+            endDate = LocalDateTime.of(
+                    LocalDate.now()
+                            .minusDays(1), LocalTime.MAX
+            )
     )
 
+
+    //start at epoch, end now
     object AllPhotos : PhotoDateFilter(
             startDate = LocalDateTime.ofEpochSecond(
                     50000, 50000,
                     ZoneOffset.UTC
-            )
+            ),
+            endDate = LocalDateTime.now()
     )
 }
