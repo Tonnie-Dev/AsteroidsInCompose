5298b999ce3052416c594e732495360f6cad3abd
Tonnie <vontonnie@gmail.com>
2024-08-05 21:02:57 +0300

    Update Plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,10 @@
-buildscript {
-    ext {
-        compose_version = '1.1.1'
-    }
-    dependencies {
-        classpath libs.hilt.android.gradle.plugin
-    }
-}// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id 'com.android.application' apply false
-    id 'com.android.library' apply false
-    id 'org.jetbrains.kotlin.android' apply false
-}
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.hilt.plugin) apply false
+    alias(libs.plugins.ksp.plugin) apply false
+    alias(libs.plugins.parcelize.plugin) apply false
+    alias(libs.plugins.compose.compiler) apply false
+
 }
