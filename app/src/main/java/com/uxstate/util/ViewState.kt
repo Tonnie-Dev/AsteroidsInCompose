1a2ff9ace02a18fcd4103a28e14440796439b7b4
Tonnie <vontonnie@gmail.com>
2024-08-06 10:38:38 +0300

    Update ViewState.kt

--- app/src/main/java/com/uxstate/util/ViewState.kt
+++ app/src/main/java/com/uxstate/util/ViewState.kt
@@ -5,8 +5,8 @@ import com.uxstate.domain.model.AstroPhoto
 
 sealed class ViewState {
     // Represents different states for the All Task screen
-    object Empty : ViewState()
-    object Loading : ViewState()
+    data object Empty : ViewState()
+    data object Loading : ViewState()
     data class Success(val photos: List<AstroPhoto>) : ViewState()
     data class Error(val exception: Throwable) : ViewState()
 }
