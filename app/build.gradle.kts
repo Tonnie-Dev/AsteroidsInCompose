d5e4acb8c7ec6a32ec31bc769913cacf8fd58d69
Tonnie <vontonnie@gmail.com>
2024-08-06 20:46:50 +0300

    Update Coil from 2.2.3 -> 2.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.uxstate.yummies"
+        applicationId = "com.uxstate"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
